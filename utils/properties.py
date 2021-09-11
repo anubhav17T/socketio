b9c16f9e8fc99573f0918eef80e944ea885161a1
anubhav17T <anubhav1tyagi@gmail.com>
2021-09-11 19:32:53 +0530

    main file modified

--- utils/properties.py
+++ utils/properties.py
@@ -4,5 +4,5 @@ DEFAULT_EXPIRE_TIME = 7 * 24 * 60 * 60 * 1000
 FILE_SUFFIXS = ("pdf", "jpeg", "png", "jpg")
 
 keysInformation ={"mongodb.auth.enabled": 'true',
-                  "mongodb.server.url": ""}
+                  "mongodb.server.url": "mongodb+srv://mollify_socketio:B5NbFuEFF09HwU3h@cluster0.mvayx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"}
 
